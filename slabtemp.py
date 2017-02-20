b719cdafa6db6a0f7acf6e3cd83d64cad6a9efca
Joseph Rippke <JoeRippke@x-10-104-127-64.uofm-secure.wireless.umn.edu>
2017-02-20 17:04:11 -0600

    added slabtemp function. eq 2.12 in McKenzie 1969

--- slabtemp.py
+++ slabtemp.py
@@ -5,4 +5,9 @@ Created on Mon Feb 20 16:33:36 2017
 
 @author: JoeRippke
 """
+import numpy as np
 
+def slabtemp(x,z,rho,Cp,vx,l,kappa):
+    R = (rho*Cp*vx*l)/(2*kappa)
+    Tprime = 1-(2/np.pi)*np.exp(-1*((np.pi**2)*x)/(2*R))*sin(np.pi*z)
+    return Tprime
