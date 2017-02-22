790d7c75a7dd812fa34c25899c5858da08ee8c97
Joe Rippke <rippk003@umn.edu>
2017-02-22 16:28:19 -0600

    modified slab temp to convert parameters to proper units and to work with main and slabmodel

--- slabtemp.py
+++ slabtemp.py
@@ -4,10 +4,15 @@
 Created on Mon Feb 20 16:33:36 2017
 
 @author: JoeRippke
+
+This module exists to contain functions needed to calculate slab temperature.
+In the future I may add functions to calculate other slab attributes.
 """
 import numpy as np
 
 def slabtemp(x,z,rho,Cp,vx,l,kappa):
-    R = (rho*Cp*vx*l)/(2*kappa)
+    v1 = vx / 31536000 #           convert cm/yr to cm/sec
+    l1 = l * 100000 #              convert km to cm
+    R = (rho*Cp*v1*l1)/(2*kappa) #  calculate Reynolds number
     Tprime = 1-(2/np.pi)*np.exp(-1*((np.pi**2)*x)/(2*R))*np.sin(np.pi*z)
     return Tprime
